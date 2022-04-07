4e34d66cd1fd43b453135def581c020a1d6f8ea6
Nedovizin Alexander <parkit74@gmail.com>
2022-04-07 23:08:27 +0500

    python account fix

--- python/socialweb/account/urls.py
+++ python/socialweb/account/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('/', include('django.contrib.auth.urls')),
 
     re_path(r'^$', views.dashboard, name='dashboard'),
-    re_path(r'^api/clients/create/$', views.register, name='register'),
-    re_path(r'^api/clients/(?P<friend_id>\d+)/match/$', views.match, name='match'),
-    re_path(r'^api/clients/edit/$', views.edit, name='edit'),
-    re_path(r'^api/list/$', views.list_members, name="list_members"),
-    re_path(r'^api/list/(?P<distance>\d+)/$', views.list_members_into_distance, name="list_members_into_distance"),
+    re_path(r'^clients/create/$', views.register, name='register'),
+    re_path(r'^clients/(?P<friend_id>\d+)/match/$', views.match, name='match'),
+    re_path(r'^clients/edit/$', views.edit, name='edit'),
+    re_path(r'^list/$', views.list_members, name="list_members"),
+    re_path(r'^list/(?P<distance>\d+)/$', views.list_members_into_distance, name="list_members_into_distance"),
 ]
